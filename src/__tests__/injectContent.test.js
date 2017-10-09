d0f3dd62db9bc98ca058e219487e9ff9122de0aa
Sputnik <inquiry@dermah.com>
2017-10-09 12:01:26 +1100

    Add text to check returned slugs are mapped correctly
    This is also an example of how to stub out methods

--- src/__tests__/injectContent.test.js
+++ src/__tests__/injectContent.test.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import {compose} from 'recompose';
 import {mount} from 'enzyme';
-import {injectContent} from '../injectContent';
 
-jest.mock('react-apollo', () => ({
-  ...jest.genMockFromModule('react-apollo'),
-  graphql: (query, options) => (WrappedComponent) => (props) =>
-    <WrappedComponent {...props} data={{loading: true, content: []}}/>,
-  // ApolloClient: jest.fn(),
-  // createNetworkInterface: jest.fn(),
-  // IntrospectionFragmentMatcher: jest.fn(),
-  // toIdValue: jest.fn()
-}))
+import {injectContent} from '../injectContent';
+import * as reactApollo from 'react-apollo';
 
 describe('injectContent', () => {
   it('is a function', () => {
@@ -19,14 +11,66 @@ describe('injectContent', () => {
   })
 
   it('injects the loading prop from react-apollo', () => {
+    const spy = jest.spyOn(reactApollo, 'graphql').mockImplementation(
+      (query, options) => (WrappedComponent) => (props) =>
+        <WrappedComponent {...props} data={{loading: true, content: []}}/>
+    )
     const fun = jest.fn(props => <div/>);
+    const Component = compose(injectContent('welcomeText'))(fun);
+    const mountedComponent = mount(
+      <Component />,
+      {context: {trxCMA: {website: 'banana'}}}
+    );
 
-    const Component = compose(injectContent('derp'))(fun);
-    const shal = mount(
+    expect(mountedComponent).toBeDefined()
+    expect(fun).toHaveBeenCalled();
+    expect(fun.mock.calls[0][0].content).toMatchObject({__loading: true});
+
+    spy.mockReset();
+    spy.mockRestore();
+  })
+
+  it('maps returned slugs directly onto the injected content prop', () => {
+    const spy = jest.spyOn(reactApollo, 'graphql').mockImplementation(
+      (query, options) => (WrappedComponent) => (props) =>
+        <WrappedComponent
+          {...props}
+          data={{
+            loading: false,
+            content: [{
+              id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
+              __typename: "TextContent",
+              "website": "banana",
+              "slug": "welcome-text",
+              "text": "Hi would you like a frozen banana"
+            }, {
+              id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
+              __typename: "TextContent",
+              "website": "banana",
+              "slug": "special-offer",
+              "text": "Free single-dip banana for family members!"
+            }]
+          }}
+        />
+    );
+    const fun = jest.fn(props => <div/>);
+    const Component = compose(
+      injectContent('welcome-text', "special-offer")
+    )(fun);
+    const mountedComponent = mount(
       <Component />,
       {context: {trxCMA: {website: 'banana'}}}
     )
-    expect(shal).toBeDefined()
-    expect(fun.mock.calls[0][0].content).toMatchObject({__loading: true});
+
+    expect(mountedComponent).toBeDefined()
+    expect(fun).toHaveBeenCalled();
+    expect(fun.mock.calls[0][0].content).toMatchObject({
+      __loading: false,
+      "welcome-text": "Hi would you like a frozen banana",
+      "special-offer": "Free single-dip banana for family members!"
+    });
+
+    spy.mockReset();
+    spy.mockRestore();
   })
 })
