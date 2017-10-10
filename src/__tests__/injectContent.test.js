738380a03fdd762c71849a7c8b235c76609a8169
Sputnik <inquiry@dermah.com>
2017-10-10 15:36:19 +1100

    Add test to confirm a list of slugs can be given to injectContent

--- src/__tests__/injectContent.test.js
+++ src/__tests__/injectContent.test.js
@@ -4,9 +4,56 @@ import {mount} from 'enzyme';
 import {injectContent} from '../injectContent';
 import * as reactApollo from 'react-apollo';
 
+const fixtures = {
+  injectedContent: [{
+    id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
+    __typename: "TextContent",
+    "website": "banana",
+    "slug": "welcome-text",
+    "text": "Hi would you like a frozen banana"
+  }, {
+    id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
+    __typename: "TextContent",
+    "website": "banana",
+    "slug": "special-offer",
+    "text": "Free single-dip banana for family members!"
+  }]
+}
+
 describe('injectContent', () => {
   it('is a function', () => {
     expect(typeof injectContent).toBe('function')
+  })
+
+  it('accepts any number of string arguments and uses apollo graphql to get slugs', () => {
+    const spy = jest.spyOn(reactApollo, 'graphql').mockImplementation(
+      (query, options) => (WrappedComponent) => (props) =>
+        <WrappedComponent
+          {...props}
+          data={{
+            loading: false,
+            content: [fixtures.injectedContent[0], fixtures.injectedContent[1]]
+          }}
+        />
+    );
+    const fun = jest.fn(props => <div/>);
+    const Component = injectContent('welcome-text', "special-offer")(fun);
+    const mountedComponent = mount(
+      <Component />,
+      {context: {trxCMA: {website: 'banana'}}}
+    )
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy.mock.calls[0][1].options({trxCMA: {website: 'banana'}})).toHaveProperty(
+      'variables.slugs',
+      [
+        {slug: "welcome-text", website: "banana"},
+        {slug: "special-offer", website: "banana"}
+      ]
+    )
+
+    spy.mockReset();
+    spy.mockRestore();
   })
 
   it('injects the loading prop from react-apollo', () => {
@@ -36,19 +83,7 @@ describe('injectContent', () => {
           {...props}
           data={{
             loading: false,
-            content: [{
-              id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
-              __typename: "TextContent",
-              "website": "banana",
-              "slug": "welcome-text",
-              "text": "Hi would you like a frozen banana"
-            }, {
-              id: "d2Vic2l0ZTp3ZWxjb21lVGV4dAo=",
-              __typename: "TextContent",
-              "website": "banana",
-              "slug": "special-offer",
-              "text": "Free single-dip banana for family members!"
-            }]
+            content: [fixtures.injectedContent[0], fixtures.injectedContent[1]]
           }}
         />
     );
